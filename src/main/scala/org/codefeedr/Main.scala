454c870c8494a7cd5d58e76f0c8a7c4d1e4d0f76
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-03 14:30:45 +0200

    Started working on ghtorrent mirror.

---
+++ src/main/scala/org/codefeedr/Main.scala
@@ -0,0 +1,50 @@
+package org.codefeedr
+
+import java.util.concurrent.TimeUnit
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies
+import org.apache.flink.api.common.time.Time
+import org.apache.flink.streaming.api.scala._
+import org.codefeedr.buffer.KafkaBuffer
+import org.codefeedr.pipeline.PipelineBuilder
+import org.codefeedr.plugins.ghtorrent.stages.{GHTInputStage, SideOutput}
+import org.codefeedr.stages.OutputStage
+import org.codefeedr.stages.utilities.{StringInput, StringType}
+
+object Main {
+
+  // sideoutput configuration
+  val sideOutput = SideOutput(true, sideOutputKafkaServer = "localhost:29092")
+
+  // all stages
+  val inputStage = new GHTInputStage("wzorgdrager")
+  val commitStage = new GHTCommitStage(sideOutput = sideOutput)
+  val commitcommentStage = new GHTCommitCommentEventStage(
+    sideOutput = sideOutput)
+  val pushStage = new GHTPushEventStage(sideOutput = sideOutput)
+  val createStage = new GHTCreateEventStage(sideOutput = sideOutput)
+  val deleteStage = new GHTDeleteEventStage(sideOutput = sideOutput)
+
+  val forkStage = new GHTForkEventStage(sideOutput = sideOutput)
+  val issueComment = new GHTIssueCommentEventStage(sideOutput = sideOutput)
+  val issueStage = new GHTIssuesEventStage(sideOutput = sideOutput)
+  val memberStage = new GHTMemberEventStage(sideOutput = sideOutput)
+  val prStage = new GHTPullRequestEventStage(sideOutput = sideOutput)
+  val prRCStage = new GHTPullRequestReviewCommentEventStage(
+    sideOutput = sideOutput)
+  val watchStage = new GHTWatchEventStage(sideOutput = sideOutput)
+
+  def main(args: Array[String]): Unit = {
+    new PipelineBuilder()
+      .setRestartStrategy(RestartStrategies.fixedDelayRestart(
+        3,
+        Time.of(10, TimeUnit.SECONDS))) // try restarting 3 times
+      .enableCheckpointing(1000) // checkpointing every 1000ms
+      .setBufferProperty(KafkaBuffer.AMOUNT_OF_PARTITIONS, "4")
+      .setBufferProperty(KafkaBuffer.COMPRESSION_TYPE, "gzip")
+      .setBufferProperty(KafkaBuffer.BROKER, "localhost:29092")
+      .setBufferProperty(KafkaBuffer.ZOOKEEPER, "localhost:2181")
+      .edge(inputStage, List())
+
+  }
+}
