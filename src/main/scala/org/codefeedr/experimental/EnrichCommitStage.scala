591db73032416a8d0648014d9432f657de60f123
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-11 13:26:48 +0200

    Wrote stream connection code with regex.

--- src/main/scala/org/codefeedr/experimental/EnrichCommitStage.scala
+++ src/main/scala/org/codefeedr/experimental/EnrichCommitStage.scala
@@ -1,18 +1,29 @@
 package org.codefeedr.experimental
 
-import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.api.scala.{DataStream, OutputTag}
 import org.codefeedr.experimental.GitHub.EnrichedCommit
 import org.codefeedr.plugins.ghtorrent.protocol.GitHub.{Commit, PushEvent}
 import org.codefeedr.stages.TransformStage2
+import org.apache.flink.api.scala._
 
 class EnrichCommitStage
     extends TransformStage2[Commit, PushEvent, EnrichedCommit] {
 
+  val unclassifiedTag = OutputTag[UnclassifiedCommit]("unclassified-data")
+
   override def transform(
       source: DataStream[Commit],
       secondSource: DataStream[PushEvent]): DataStream[EnrichedCommit] = {
-    source.keyBy()
+    source.keyBy { x =>
+      val regex = """repos\/([^\/]*\/[^\/]*)""".r
 
-    secondSource.keyBy()
+      regex.findFirstIn(x.url).get.replace("repos/", "")
+    }
+
+    secondSource.keyBy(_.repo.name)
+
+    secondSource
+      .connect(source)
+      .process(new EnrichCommitProcess(unclassifiedTag))
   }
 }
