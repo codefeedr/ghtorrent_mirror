fb8b1d34f5e89b670e3cd0995dbb54afe187666c
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-11 14:21:29 +0200

    Side output to Kafka instead of stderr

--- src/main/scala/org/codefeedr/experimental/EnrichCommitStage.scala
+++ src/main/scala/org/codefeedr/experimental/EnrichCommitStage.scala
@@ -1,13 +1,25 @@
 package org.codefeedr.experimental
 
+import java.util.Properties
+
 import org.apache.flink.streaming.api.scala.{DataStream, OutputTag}
 import org.codefeedr.experimental.GitHub.EnrichedCommit
 import org.codefeedr.plugins.ghtorrent.protocol.GitHub.{Commit, PushEvent}
 import org.codefeedr.stages.TransformStage2
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
+import org.codefeedr.buffer.serialization.Serializer
+import org.codefeedr.plugins.ghtorrent.protocol.GHTorrent.Record
+import org.codefeedr.plugins.ghtorrent.stages.SideOutput
 
-class EnrichCommitStage(stageId: String = "cf_commit")
+class EnrichCommitStage(stageId: String = "cf_commit",
+                        sideOutput: SideOutput = SideOutput())
     extends TransformStage2[Commit, PushEvent, EnrichedCommit](Some(stageId)) {
+
+  // We only send to this topic once in a while, so we need to disable batches.
+  val props = new Properties()
+  props.put("bootstrap.servers", sideOutput.sideOutputKafkaServer)
+  props.put("batch.size", "0")
 
   val unclassifiedTag = OutputTag[UnclassifiedCommit]("unclassified-data")
 
@@ -33,7 +45,11 @@ class EnrichCommitStage(stageId: String = "cf_commit")
     /** Print side-output as error. */
     secondSource
       .getSideOutput(unclassifiedTag)
-      .printToErr()
+      .addSink(
+        new FlinkKafkaProducer[UnclassifiedCommit](
+          sideOutput.sideOutputTopic,
+          Serializer.getSerde[UnclassifiedCommit](Serializer.JSON),
+          props))
 
     processed
   }
