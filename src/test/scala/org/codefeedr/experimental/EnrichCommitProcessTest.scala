b18f360b036f8b147748ceb68bb9788b9e5e8998
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-12 16:01:19 +0200

    Fix the test as well.

--- src/test/scala/org/codefeedr/experimental/EnrichCommitProcessTest.scala
+++ src/test/scala/org/codefeedr/experimental/EnrichCommitProcessTest.scala
@@ -6,12 +6,12 @@ import org.codefeedr.experimental.TestProtocol._
 class EnrichCommitProcessTest extends FunSuite {
 
   test("GitHub commit should be correctly verified.") {
-    val enrichCommitProcess = new EnrichCommitProcess(null)
+    val enrichCommitProcess = new EnrichCommitProcess()
     assert(enrichCommitProcess.pushedFromGitHub(verifiedCommit))
   }
 
   test("GitHub commit could also not be verified.") {
-    val enrichCommitProcess = new EnrichCommitProcess(null)
+    val enrichCommitProcess = new EnrichCommitProcess()
 
     assert(!enrichCommitProcess.pushedFromGitHub(unverifiedCommit))
   }
