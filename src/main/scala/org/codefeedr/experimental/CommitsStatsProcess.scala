9d8aaf346a203de940cc0c1526010af0dbaa4f6c
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-15 11:49:21 +0200

    Initialized state

--- src/main/scala/org/codefeedr/experimental/CommitsStatsProcess.scala
+++ src/main/scala/org/codefeedr/experimental/CommitsStatsProcess.scala
@@ -1,5 +1,11 @@
 package org.codefeedr.experimental
 
+import org.apache.flink.api.common.state.{
+  StateTtlConfig,
+  ValueState,
+  ValueStateDescriptor
+}
+import org.apache.flink.api.common.time.Time
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.util.Collector
@@ -8,9 +14,15 @@ import org.codefeedr.plugins.ghtorrent.protocol.GitHub.Commit
 
 class CommitsStatsProcess extends ProcessFunction[Commit, Stats] {
 
-  override def open(parameters: Configuration): Unit = {}
+  private var statsState: ValueState[Stats] = _
+
+  override def open(parameters: Configuration): Unit = {
+    val statsStateDescriptor =
+      new ValueStateDescriptor[Stats]("stats", classOf[Stats])
+    statsState = getRuntimeContext.getState[Stats](statsStateDescriptor)
+  }
 
   override def processElement(value: Commit,
                               ctx: ProcessFunction[Commit, Stats]#Context,
-                              out: Collector[Stats]): Unit = ???
+                              out: Collector[Stats]): Unit = {}
 }
