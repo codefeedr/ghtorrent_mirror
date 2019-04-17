e86f3a5207b69075a12293fe35faa19515d9d9ee
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 10:06:52 +0200

    Almost done.

--- src/main/scala/org/codefeedr/experimental/stats/CommitsStatsStage.scala
+++ src/main/scala/org/codefeedr/experimental/stats/CommitsStatsStage.scala
@@ -13,24 +13,28 @@ import org.apache.flink.streaming.api.windowing.triggers.{
   ProcessingTimeTrigger,
   PurgingTrigger
 }
-import org.codefeedr.experimental.stats.StatsObjects.Stats
 
+/** Stage which reduces stats and sends to MongoDB. */
 class CommitsStatsStage(name: String = "commit_stats")
     extends OutputStage[Commit](Some(name)) {
 
+  /** Processes the input stream of commits.
+    *
+    * @param source the source to read from.
+    */
   override def main(source: DataStream[Commit]): Unit = {
-    // We wan't to consider the processing time.
+    // We want to consider the processing time.
     this.getContext.env
       .setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
 
     source
-      .keyBy(new KeyOnDate)
-      .process(new CommitsStatsProcess)
-      .keyBy(_._2.date)
-      .timeWindow(Time.minutes(10))
-      .trigger(PurgingTrigger.of(ProcessingTimeTrigger.create()))
-      .aggregate(new EmitHighestTimestamp)
-      .addSink(
+      .keyBy(new KeyOnDate) // key on hourly date.
+      .process(new CommitsStatsProcess) // reduce it.
+      .keyBy(_._2.date) // key again on this hourly date.
+      .timeWindow(Time.minutes(10)) // every 10 minutes.
+      .trigger(PurgingTrigger.of(ProcessingTimeTrigger.create())) // we trigger on processing time and purge the window.
+      .aggregate(new EmitHighestTimestamp) // and emit the stats object with highest timestamp.
+      .addSink( // finally we send this to mongodb.
         new InsertAndReplaceMongoSink(
           Map("server" -> "mongodb://localhost:27017",
               "database" -> "codefeedr",
@@ -39,6 +43,7 @@ class CommitsStatsStage(name: String = "commit_stats")
   }
 }
 
+/** Parses date to hourly and key by it. */
 class KeyOnDate extends KeySelector[Commit, String] {
   private lazy val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH")
 
