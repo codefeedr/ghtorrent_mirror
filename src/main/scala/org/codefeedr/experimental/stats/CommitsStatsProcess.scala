faf4b17ee697cb4e99fad24a2fa10a572149e277
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 09:52:38 +0200

    Comments!

--- src/main/scala/org/codefeedr/experimental/stats/CommitsStatsProcess.scala
+++ src/main/scala/org/codefeedr/experimental/stats/CommitsStatsProcess.scala
@@ -18,8 +18,13 @@ class CommitsStatsProcess
     extends KeyedProcessFunction[String, Commit, (Long, Stats)] {
   private var statsState: ValueState[Stats] = _
 
+  /** Count all the commits processed. */
   private val sumCommits = new LongCounter()
 
+  /** Open this ProcessFunction by creating an accumulator and create state.
+    *
+    * @param parameters the configuration.
+    */
   override def open(parameters: Configuration): Unit = {
     val statsStateDescriptor =
       new ValueStateDescriptor[Stats]("stats", classOf[Stats])
@@ -28,12 +33,19 @@ class CommitsStatsProcess
     getRuntimeContext.addAccumulator("total_commits_processed", sumCommits)
   }
 
+  /** Process an element and reduce to a stats object.
+    *
+    * @param value the commit to reduce.
+    * @param ctx the context.
+    * @param out the collector.
+    */
   override def processElement(
       value: Commit,
       ctx: KeyedProcessFunction[String, Commit, (Long, Stats)]#Context,
       out: Collector[(Long, Stats)]): Unit = {
     val currentStats = statsState.value()
 
+    // If there is no state, create it.
     if (currentStats == null) {
       val stats = createStats(ctx.getCurrentKey, value)
 
@@ -50,12 +62,20 @@ class CommitsStatsProcess
       out.collect(ctx.timerService().currentProcessingTime(), newStats)
     }
 
+    // Increment accumulator.
     sumCommits.add(1)
   }
 
+  /** Creates a stats object based on a Commit.
+    *
+    * @param key the key (which is the date).
+    * @param commit the commit to create it from.
+    * @return a Stats object.
+    */
   def createStats(key: String, commit: Commit): Stats = {
     val date = key
 
+    // For all files merge an extension list.
     val extensions = commit.files
       .map { file =>
         var extension = "unknown"
@@ -91,6 +111,12 @@ class CommitsStatsProcess
     Stats(date, CommitStats(1, extensions))
   }
 
+  /** Merges two extensions.
+    *
+    * @param a the first extension to merge.
+    * @param b the second extension to merge.
+    * @return the merged extension.
+    */
   def mergeExtension(a: Extension, b: Extension): Extension = (a, b) match {
     case (Extension(nameA, addiA, deliA, addA, remA, modA),
           Extension(nameB, addiB, deliB, addB, remB, modB)) if nameA == nameB =>
@@ -105,10 +131,17 @@ class CommitsStatsProcess
         "Cannot merge two extensions because they have different names.")
   }
 
+  /** Merges two stats objects into one.
+    *
+    * @param statsA the first stats object.
+    * @param statsB the second stats object.
+    * @return A merged stats object.
+    */
   def merge(statsA: Stats, statsB: Stats): Stats = {
     val extensionsA = statsA.commitStats.filesEdited
     val extensionsB = statsB.commitStats.filesEdited
 
+    // FP magic!
     val extensions = (extensionsA ::: extensionsB)
       .groupBy(_.name)
       .mapValues(_.reduce(mergeExtension))
