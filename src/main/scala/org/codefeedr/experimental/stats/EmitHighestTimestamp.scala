01e5cde837342993c7d287c60e40762e98f8d010
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 10:12:41 +0200

    Final scala doc.

--- src/main/scala/org/codefeedr/experimental/stats/EmitHighestTimestamp.scala
+++ src/main/scala/org/codefeedr/experimental/stats/EmitHighestTimestamp.scala
@@ -4,17 +4,23 @@ import org.apache.flink.api.common.functions.AggregateFunction
 
 import org.codefeedr.experimental.stats.StatsObjects.{CommitStats, Stats}
 
+/** Simple aggregator which only saves the latest timestamp. */
 class EmitHighestTimestamp
     extends AggregateFunction[(Long, Stats), (Long, Stats), Stats] {
+
+  /** Creates empty accumulator with lowest timestamp. */
   override def createAccumulator(): (Long, Stats) =
     (0, Stats("", CommitStats(0L, List())))
 
+  /** 'Adds' by picking the one with highest timestamp. */
   override def add(value: (Long, Stats),
                    accumulator: (Long, Stats)): (Long, Stats) =
     if (value._1 > accumulator._1) value else accumulator
 
+  /** Gets the final stats object. */
   override def getResult(accumulator: (Long, Stats)): Stats = accumulator._2
 
+  /** Merges two aggregators by picking the one with highest timestamp. */
   override def merge(a: (Long, Stats), b: (Long, Stats)): (Long, Stats) =
     if (a._1 > b._1) a else b
 }
--- src/main/scala/org/codefeedr/experimental/stats/InsertAndReplaceMongoSink.scala
+++ src/main/scala/org/codefeedr/experimental/stats/InsertAndReplaceMongoSink.scala
@@ -17,22 +17,33 @@ import org.mongodb.scala.model.ReplaceOptions
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+/** Inserts stats into MongoDB. */
 class InsertAndReplaceMongoSink(val userConfigg: Map[String, String])
     extends BaseMongoSink[Stats](userConfigg) {
 
+  /** Called for every stats object.
+    * The _id is set equal to date, so that it will be overriden in case of an update.
+    *
+    * @param value the stats object.
+    * @param context the sink context.
+    */
   override def invoke(value: Stats, context: SinkFunction.Context[_]): Unit = {
     val collection = getCollection
 
+    // Set _id == date.
     val json = Serialization.write(value)(formats)
     val doc = Document(json)
     doc += "_id" -> value.date
 
+    // Make sure it replaces or inserts.
     val replaceOptions = new ReplaceOptions()
     replaceOptions.upsert(true)
 
+    // Let's go.
     val result =
       collection.replaceOne(equal("_id", value.date), doc, replaceOptions)
 
+    // And wait.
     Await.result(result.toFuture, Duration.Inf)
   }
 }
